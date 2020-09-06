0b3d3e49b51322e47c7cce1b2c88b78b36d2a355
Oleksandr Bieliakov <abel9k@gmail.com>
2020-09-06 17:19:26 +0200

    Lesson 10 - android test dependencies fixed

--- Design/app/src/androidTest/java/com/example/android/gdgfinder/ExampleInstrumentedTest.kt
+++ Design/app/src/androidTest/java/com/example/android/gdgfinder/ExampleInstrumentedTest.kt
@@ -1,8 +1,8 @@
 package com.example.android.gdgfinder
 
 import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
-import org.junit.Assert.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
